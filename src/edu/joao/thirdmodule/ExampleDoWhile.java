2f9240d5afa01fd358b10c9db11b5ac55c98fc8a
Joao Bogo <joaoluciano9812@gmail.com>
2023-08-08 14:11:50 -0300

    u

---
+++ src/edu/joao/thirdmodule/ExampleDoWhile.java
@@ -0,0 +1,21 @@
+package edu.joao.thirdmodule;
+
+import java.util.Random;
+
+public class ExampleDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do {
+            System.out.println("Telefone Tocando");
+        } while (tocando());
+
+        System.out.println("Alo!!");
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        return ! atendeu;
+    }
+}
--- src/edu/joao/thirdmodule/ForExampleArrays.java
+++ src/edu/joao/thirdmodule/ForExampleArrays.java
@@ -4,8 +4,7 @@ public class ForExampleArrays {
     
     public static void main(String[] args) {
         String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
-        for (int i=0 ; i<alunos.length; ++i){
-            System.out.println("O aluno(a) no indice i= " + i + " é " + alunos[i]);
-
-    }
+      for(String aluno: alunos){
+        System.out.println("Nome do aluno é: " + aluno);
+      }
     }}
